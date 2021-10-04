608dca0380f843514777a3f6b0a0ba1a2d5f8801
Vo1z <vitya.voody@gmail.com>
2021-10-04 14:24:00 +0200

    dev 11
    -Charges now can be disabled

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -7,10 +7,13 @@ public class PlayerData : ScriptableObject
     [SerializeField] [Min(0)] private float dashForce = 50;
     [SerializeField] [Min(0)] private float afterDashForce = 10;
     [SerializeField] [Min(0)] private float maxDashDistance = 10;
-    [Header("Charge options")]
+
+    [Header("Charge options")] [SerializeField]
+    private bool areChargesUsed = true;
     [SerializeField] [Min(0)] private int initialNumberOfCharges = 5;
     [SerializeField] [Min(0)] private float chargeRegenerationTime = 1f;
 
+    public bool AreChargesUsed => areChargesUsed;
     public float DashForce => dashForce;
     public float AfterDashForce => afterDashForce;
     public float MaxDashDistance => maxDashDistance;
