ca344adefa4f9d4cb30812fc5e67d792b8e4cd80
Ivan <iv_fomenko@bk.ru>
2021-10-11 19:42:23 +0200

    dev 16
    -Included Resharper's .dotsettings in ,gitignore
    -Implemented basic screen shake with cinemachine

---
+++ Assets/Scripts/Graphics/ScreenShake.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using Cinemachine;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+    [SerializeField] [Min(0.0f)] private float amplitudeGain;
+    [SerializeField] [Min(0.0f)] private float frequencyGain;
+    [SerializeField] [Min(0.0f)] private float shakeDuration;
+
+    private Vector3 _cameraPosition;
+    private Quaternion _cameraRotation;
+
+
+    private void Awake()
+    {
+        var transform1 = this.transform;
+        var position = transform1.position;
+        var rotation = transform1.rotation;
+        _cameraPosition = new Vector3(position.x, position.y, position.z);
+        _cameraRotation = new Quaternion(rotation.x, rotation.y, rotation.z, rotation.w);
+        cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = frequencyGain;
+    }
+
+    private IEnumerator ScreenShakeRoutine()
+    {
+        var timeElapsed = 0.0f;
+        var cinemachineBasicMultiChannelPerlin =
+            cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        while (true)
+        {
+            cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 
+                Mathf.Lerp(amplitudeGain, 0.0f, timeElapsed/shakeDuration);
+
+            timeElapsed += Time.deltaTime;
+            if (timeElapsed >= shakeDuration)
+                break;
+            
+            yield return new WaitForEndOfFrame();
+        }
+
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 0.0f;
+        ResetCameraTransform();
+        yield return null;
+    }
+
+    private void ResetCameraTransform()
+    {
+        var transform1 = transform;
+        transform1.position = _cameraPosition;
+        transform1.rotation = _cameraRotation;
+    }
+
+    private void StartScreenShake()
+    {
+        StopAllCoroutines();
+        StartCoroutine(ScreenShakeRoutine());
+    }
+}
