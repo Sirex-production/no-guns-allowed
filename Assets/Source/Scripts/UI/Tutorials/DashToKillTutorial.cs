1f61957a79c5e9238d3afdfb6694ee859154b9a8
Viktor Lishchuk <vitya.voody@gmail.com>
2022-03-03 22:33:55 +0100

    voiz-tutorial 4
    -Dash to kill tutorial was set up
    -Changes on tutorial level

--- Assets/Source/Scripts/UI/Tutorials/DashToKillTutorial.cs
+++ Assets/Source/Scripts/UI/Tutorials/DashToKillTutorial.cs
@@ -1,5 +1,7 @@
 using DG.Tweening;
 using Extensions;
+using ModestTree;
+using NaughtyAttributes;
 using Support;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,17 +11,30 @@ namespace Ingame.UI
 {
     public class DashToKillTutorial : MonoTutorial
     {
+        [BoxGroup("References"), Required]
         [SerializeField] private Image playerImage;
+        [BoxGroup("References"), Required]
         [SerializeField] private Image enemyImage;
+        [BoxGroup("References"), Required]
         [SerializeField] private Image bloodImage;
+        [BoxGroup("References"), Required]
         [SerializeField] private CanvasGroup parentCanvas;
         [Space] 
+        [BoxGroup("Animation properties")]
         [SerializeField] [Min(0)] private float animationDuration = 1;
+        [BoxGroup("Animation properties")]
         [SerializeField] [Min(0)] private float dashAnimationDuration = .1f;
+        [BoxGroup("Animation properties")]
         [SerializeField] [Min(0)] private float bloodImageScaleMultiplier = 1.2f;
+        [Space]
+        [BoxGroup("Game properties"), Tooltip("Message that will be displayed to the LOG window when tutorial is activated")]
+        [SerializeField] private string activateLogMessage;
+        [BoxGroup("Game properties"), Tooltip("Message that will be displayed to the LOG window when tutorial is completed")]
+        [SerializeField] private string completeLogMessage;
 
         [Inject] private GameController _gameController;
         [Inject] private TutorialsManager _tutorialsManager;
+        [Inject] private UiController _uiController;
         
         private Vector3 _initialBloodImageScale;
         private Sequence _animationSequence;
@@ -67,6 +82,9 @@ namespace Ingame.UI
 
         public override void Complete()
         {
+            if(activateLogMessage != null || !activateLogMessage.IsEmpty()) 
+                _uiController.DisplayLogMessage(completeLogMessage, LogDisplayType.DisplayAndKeep);
+            
             if(_animationSequence != null)
                 _animationSequence.Kill();
 
@@ -75,6 +93,9 @@ namespace Ingame.UI
 
         public override void Activate()
         {
+            if(activateLogMessage != null || !activateLogMessage.IsEmpty()) 
+                _uiController.DisplayLogMessage(activateLogMessage, LogDisplayType.DisplayAndKeep);
+            
             playerImage.DOFade(0, 0);
             bloodImage.DOFade(0, 0);
             enemyImage.DOFade(0, 0);
