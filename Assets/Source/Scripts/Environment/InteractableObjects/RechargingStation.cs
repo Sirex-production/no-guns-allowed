86d8100a43490f78bf9a5d73bc7c00a54db8c9ea
Ivan <iv_fomenko@bk.ru>
2022-03-05 21:51:14 +0100

    dev 112
    -Deleted redundant materials
    -Revamped recharging stations with property blocks

--- Assets/Source/Scripts/Environment/InteractableObjects/RechargingStation.cs
+++ Assets/Source/Scripts/Environment/InteractableObjects/RechargingStation.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using Extensions;
 using Ingame;
 using Ingame.AI;
@@ -18,29 +19,41 @@ public class RechargingStation : MonoBehaviour
     [Header("Station options")]
     [SerializeField] private int regenerationSpeedMultiplier;
     [SerializeField] private float cooldown;
+
     [Header("Ui properties")] 
     [SerializeField] private string logMessageWhenEntered;
-    [Header("State Meshes")] 
-    [SerializeField] private MeshRenderer activeMesh;
-    [SerializeField] private MeshRenderer inactiveMesh;
-    [SerializeField] private MeshRenderer cooldownMesh;
-    
+
+    [Header("Visual options")] 
+    [SerializeField] private float transitionTime;
+    [SerializeField] [ColorUsage(false, true)] private Color activeColor;
+    [SerializeField] [ColorUsage(false, true)] private Color inactiveColor;
+    [SerializeField] [ColorUsage(false, true)] private Color cooldownColor;
+
+    [Header("References")] [SerializeField]
+    private List<ParticleSystem> particleSystems;
+
     [Inject] private UiController _uiController;
 
+    private static readonly int EMISSION_COLOR = Shader.PropertyToID("EmissionColor");
+
     private PlayerEventController _playerEventController;
+    private Renderer _renderer;
+    private MaterialPropertyBlock _propertyBlock;
     private State _state;
 
     private void Awake()
     {
+        _renderer = GetComponent<Renderer>();
+        _propertyBlock = new MaterialPropertyBlock();
         _state = State.Inactive;
-        inactiveMesh.SetGameObjectActive();
-
-        activeMesh.SetGameObjectInactive();
-        cooldownMesh.SetGameObjectInactive();
     }
 
     private void Start()
     {
+        _renderer.GetPropertyBlock(_propertyBlock);
+        _propertyBlock.SetColor(EMISSION_COLOR, inactiveColor);
+        _renderer.SetPropertyBlock(_propertyBlock);
+
         _playerEventController = PlayerEventController.Instance;
     }
 
@@ -68,6 +81,22 @@ public class RechargingStation : MonoBehaviour
         SwitchState(State.Active);
     }
 
+    private IEnumerator ColorShiftRoutine(Color currentColor, Color newColor)
+    {
+        var timeLeft = transitionTime;
+
+        while (timeLeft > 0)
+        {
+            var timeClamped = 1 - timeLeft / transitionTime;
+            _renderer.GetPropertyBlock(_propertyBlock);
+            _propertyBlock.SetColor(EMISSION_COLOR, Color.Lerp(currentColor, newColor, timeClamped));
+            _renderer.SetPropertyBlock(_propertyBlock);
+
+            timeLeft -= Time.deltaTime * Time.timeScale;
+            yield return null;
+        }
+    }
+
     private IEnumerator CooldownRoutine()
     {
         if(_state != State.OnCooldown)
@@ -86,39 +115,47 @@ public class RechargingStation : MonoBehaviour
         return otherIsNotThePlayer || _state != expectedState;
     }
 
+
+    /// <param name="state">true when launching, false when stopping</param>
+    private void ToggleParticleSystems(bool state)
+    {
+        if (state)
+            foreach (var system in particleSystems)
+                system.Play();
+
+        else
+            foreach (var system in particleSystems)
+                system.Stop();
+    }
+
+    private void ShiftColor(Color newColor)
+    {
+        StopCoroutine(nameof(ColorShiftRoutine));
+
+        _renderer.GetPropertyBlock(_propertyBlock);
+        var currentColor = _propertyBlock.GetColor(EMISSION_COLOR);
+        StartCoroutine(ColorShiftRoutine(currentColor, newColor));
+    }
+
     private void SwitchState(State state)
     {
-        switch (_state)
-        {
-            case State.Active:
-                activeMesh.SetGameObjectInactive();
-                break;
-            
-            case State.Inactive:
-                inactiveMesh.SetGameObjectInactive();
-                break;
-            
-            case State.OnCooldown:
-                cooldownMesh.SetGameObjectInactive();
-                break;
-        }
-
         _state = state;
-
         switch (_state)
         {
             case State.Active:
-                activeMesh.SetGameObjectActive();
+                ToggleParticleSystems(true);
+                ShiftColor(activeColor);
                 _uiController.DisplayLogMessage(logMessageWhenEntered, LogDisplayType.DisplayAndClear);
                 _playerEventController.StatsController.ChargeRegenerationTimeModifier = regenerationSpeedMultiplier;
                 break;
             
             case State.Inactive:
-                inactiveMesh.SetGameObjectActive();
+                ShiftColor(inactiveColor);
                 break;
             
             case State.OnCooldown:
-                cooldownMesh.SetGameObjectActive();
+                ToggleParticleSystems(false);
+                ShiftColor(cooldownColor);
                 StartCoroutine(CooldownRoutine());
                 _playerEventController.StatsController.ChargeRegenerationTimeModifier = 1;
                 break;
