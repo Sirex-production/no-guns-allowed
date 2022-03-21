7d862524237c5e864aa7034b2b49f9aa03fab6ad
Ivan <iv_fomenko@bk.ru>
2022-03-21 21:55:41 +0100

    dev 172
    -Cleared redundant code from 169

--- Assets/Source/Scripts/Environment/InteractableObjects/Invokable/InvokableButton.cs
+++ Assets/Source/Scripts/Environment/InteractableObjects/Invokable/InvokableButton.cs
@@ -15,30 +15,15 @@ namespace Ingame
     public class InvokableButton : MonoBehaviour
     {
         [SerializeField] private MonoInvokable[] invokableObjects;
-        [SerializeField] private Renderer mesh;
         [SerializeField] private bool deactivatePanelAfterInteraction = false;
         [Tooltip("Text that will be displayed in log when player approaches the button")]
         [SerializeField] private string logApproachText;
         [Tooltip("Text that will be displayed in log when player interacts with the button")]
         [SerializeField] private string logInteractionText;
-        [Tooltip("Control panel color after interaction")]
-        [ColorUsage(false, true)]
-        [SerializeField] private Color activeColor;
 
         [Inject] private UiController _uiController;
 
-        private Color _inactiveColor;
-        private MaterialPropertyBlock _propertyBlock;
-        private bool _isActivated = false;
         private bool _isWorking = true;
-        private static readonly int EMISSION_COLOR = Shader.PropertyToID("_EmissionColor");
-
-        private void Awake()
-        {
-            _propertyBlock = new MaterialPropertyBlock();
-            mesh.GetPropertyBlock(_propertyBlock);
-            _inactiveColor = _propertyBlock.GetColor(EMISSION_COLOR);
-        }
 
         private void OnTriggerEnter(Collider other)
         {
@@ -101,18 +86,8 @@ namespace Ingame
                 invokableObject.Invoke();
             }
 
-            _isActivated = !_isActivated;
-            SwitchColor();
-
             if (deactivatePanelAfterInteraction)
                 _isWorking = false;
-        }
-
-        private void SwitchColor()
-        {
-            mesh.GetPropertyBlock(_propertyBlock);
-            _propertyBlock.SetColor(EMISSION_COLOR, _isActivated ? activeColor : _inactiveColor);
-            mesh.SetPropertyBlock(_propertyBlock);
         }
     }
 }
