3fb1ed0a656206b6f0174bea15cc286ac42d5e9b
Ivan <iv_fomenko@bk.ru>
2022-03-27 02:12:17 +0100

    dev 205
    -Cleared all afterimage related functionality from "TrailController" 
    -Removed trail controller component from player prefab

--- Assets/Source/Scripts/Graphics/TrailController.cs
+++ Assets/Source/Scripts/Graphics/TrailController.cs
@@ -9,31 +9,21 @@ namespace Ingame.Graphics
     [RequireComponent(typeof(PlayerMovementController))]
     public class TrailController : MonoBehaviour
     {
-        [SerializeField] private float spawnPeriod;
-        [SerializeField] private GameObject ghostPrefab;
         [SerializeField] private TrailRenderer primaryTrail;
         [SerializeField] private List<TrailRenderer> secondaryTrails;
 
-        [Inject] private PoolManager _poolManager;
-        
-        // Start is called before the first frame update
         private void Start()
         {
-            if(primaryTrail != null)
-                primaryTrail.emitting = false;
-            foreach (var trail in secondaryTrails)
-                trail.emitting = true;
+            SwitchActiveTrails(false);
 
-            _poolManager.CreatePool(ghostPrefab, 7);
-            
-            PlayerEventController.Instance.OnDashPerformed += StartSpawningGhosts;
-            PlayerEventController.Instance.OnDashStop += StopSpawningGhosts;
+            PlayerEventController.Instance.OnDashPerformed += StartPrimaryTrail;
+            PlayerEventController.Instance.OnDashStop += StopPrimaryTrail;
         }
 
         private void OnDestroy()
         {
-            PlayerEventController.Instance.OnDashPerformed -= StartSpawningGhosts;
-            PlayerEventController.Instance.OnDashStop -= StopSpawningGhosts;
+            PlayerEventController.Instance.OnDashPerformed -= StartPrimaryTrail;
+            PlayerEventController.Instance.OnDashStop -= StopPrimaryTrail;
         }
 
         private void SwitchActiveTrails(bool primaryTrailEmittingValue)
@@ -41,31 +31,18 @@ namespace Ingame.Graphics
             if(primaryTrail != null)
                 primaryTrail.emitting = primaryTrailEmittingValue;
             foreach (var trail in secondaryTrails)
-                trail.emitting = !trail.emitting;
+                trail.emitting = !primaryTrailEmittingValue;
         }
 
-        private void StartSpawningGhosts(Vector3 _)
-        {
-            StartCoroutine(SpawnGhostsRoutine());
-        }
-
-        private void StopSpawningGhosts()
-        {
-            SwitchActiveTrails(false);
-            StopAllCoroutines();
-        }
-
-
-        private IEnumerator SpawnGhostsRoutine()
+        private void StartPrimaryTrail(Vector3 _)
         {
             SwitchActiveTrails(true);
-            while (true)
-            {
-                _poolManager.ReuseObject(ghostPrefab, this.transform.position);
 
-                yield return new WaitForSeconds(spawnPeriod);
-            }
-            // ReSharper disable once IteratorNeverReturns
+        }
+
+        private void StopPrimaryTrail()
+        {
+            SwitchActiveTrails(false);
         }
     }
 }
